9364d983f71001b0409a4e525536405674c7ecc6
Daniel Petri <daniel.petri@rwth-aachen.de>
2020-07-07 16:42:11 +0200

    Apply SECodeFormatter.xml

--- petrinet-tests-junit-generator/src/test/java/petrinettests/junitgenerator/BasicGeneratorTest.java
+++ petrinet-tests-junit-generator/src/test/java/petrinettests/junitgenerator/BasicGeneratorTest.java
@@ -1,8 +1,6 @@
 package petrinettests.junitgenerator;
 
-
 import org.junit.jupiter.api.Test;
-
 import petrinettests._ast.ASTPetriNetTest;
 import petrinettests._parser.PetrinetTestsParser;
 
@@ -12,27 +10,24 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BasicGeneratorTest {
-    @Test
-    public void testBasicSim() throws IOException {
-        PetrinetTestsParser parser = new PetrinetTestsParser();
-        Optional<ASTPetriNetTest> ast = parser.parse("src/test/resources/test01.pnt");
-        assertTrue(ast.isPresent());
-        ASTPetriNetTest test = ast.get();
+  @Test
+  public void testBasicSim() throws IOException {
+    PetrinetTestsParser parser = new PetrinetTestsParser();
+    Optional<ASTPetriNetTest> ast = parser.parse("src/test/resources/test01.pnt");
+    assertTrue(ast.isPresent());
+    ASTPetriNetTest test = ast.get();
 
-        JUnitGenerator.generateJUnit(test);
-    }
+    JUnitGenerator.generateJUnit(test);
+  }
 
+  @Test
+  public void testDefinedMarkingWithRest() throws IOException {
+    PetrinetTestsParser parser = new PetrinetTestsParser();
+    Optional<ASTPetriNetTest> ast = parser.parse("src/test/resources/test02.pnt");
+    assertTrue(ast.isPresent());
+    ASTPetriNetTest test = ast.get();
 
-    @Test
-    public void testDefinedMarkingWithRest() throws IOException {
-        PetrinetTestsParser parser = new PetrinetTestsParser();
-        Optional<ASTPetriNetTest> ast = parser.parse("src/test/resources/test02.pnt");
-        assertTrue(ast.isPresent());
-        ASTPetriNetTest test = ast.get();
-
-        JUnitGenerator.generateJUnit(test);
-    }
-
-
+    JUnitGenerator.generateJUnit(test);
+  }
 
 }
